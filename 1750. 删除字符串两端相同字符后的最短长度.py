89e036d9d18e322403e7c3016054d646ae59bb3e
jgw <jgw__jgw@163.com>
2022-03-17 17:06:19 +0800

    [2022-03-17] 递归直接超时，最后用的双指针，但是有待回顾，毕竟抄的作业……

--- 1750. 删除字符串两端相同字符后的最短长度.py
+++ 1750. 删除字符串两端相同字符后的最短长度.py
@@ -30,5 +30,82 @@ s 只包含字符 'a'，'b' 和 'c' 。
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 
+"""
+双指针
+"""
+
+
+class Solution:
+    @staticmethod
+    def is_deletable(s: str, n: int) -> bool:
+        return n > 1 and s[0] == s[-1]
+
+    @staticmethod
+    def get_left_continuous_length(s: str, n: int) -> int:
+        count = 1
+        while count < n:
+            if s[count] == s[0]:
+                count += 1
+            else:
+                break
+
+        return count
+
+    @staticmethod
+    def get_right_continuous_length(s: str, n: int) -> int:
+        index = n - 2
+        while index >= 0:
+            if s[index] == s[n - 1]:
+                index -= 1
+            else:
+                break
+
+        return n - index - 1
+
+    def minimumLength(self, s: str) -> int:
+        n = len(s)
+        if not self.is_deletable(s, n):
+            return n
+
+        # 求左侧连续的相同字符的长度
+        left_count = self.get_left_continuous_length(s, n)
+
+        # 如果这个长度等于 n，说明 s 由相同的字符构成，一定可以删掉
+        if left_count == n:
+            return 0
+
+        # 求右侧连续的相同字符的长度
+        right_count = self.get_right_continuous_length(s, n)
+
+        return self.minimumLength(s[left_count:(n - right_count)])
+
+
+class Solution:
+    @staticmethod
+    def minimumLength(s: str) -> int:
+        left_pointer, right_pointer = 0, len(s) - 1
+        while left_pointer < right_pointer:
+            if s[left_pointer] != s[right_pointer]:
+                break
+            char = s[left_pointer]
+            while left_pointer <= right_pointer and s[left_pointer] == char:
+                left_pointer += 1
+            while left_pointer <= right_pointer and s[right_pointer] == char:
+                right_pointer -= 1
+
+        return right_pointer - left_pointer + 1
+
+
 if __name__ == '__main__':
+    il = [
+        'ca',
+        'cabaabac',
+        'aabccabba',
+        # 'aa',
+    ]
+
+    s = Solution()
+
+    for item in il:
+        pprint(s.minimumLength(item))
     pass
